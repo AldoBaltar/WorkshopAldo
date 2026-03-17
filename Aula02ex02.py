fa990b7ad3380c699596a364aa84f9393ac60d06
AldoBaltar <aldobv.neto@gmail.com>
2026-03-17 16:03:44 -0300

    inutil

--- Aula02ex02.py
+++ Aula02ex02.py
@@ -1,1 +1,35 @@
+
+
+class Calculadora:
+    def __init__(self, x, y):
+            self.num1 = x
+            self.num2= y
+
+    def main(self):
+        print("""
+[1] Soma
+[2] Subtração 
+[3] Multiplicação
+[4] Divisão
+[5] Encerrar
+    """)
+        base = int(input('Digite a operação desejada: '))
+        x = float(input('Digite um numero: '))
+        y = float(input('Digite outro numero: '))
+        print(f'A {soma} de {x} e {y}')
+
+    def soma(self):
+        print(self.num1 +  self.num2)
+        
+    def subtracao(self):
+        print(self.num1 -  self.num2)
     
+    def multiplicacao(self):
+        print(self.num1 *  self.num2)
+    
+    def divisao(self):
+        print(self.num1 /  self.num2)
+
+
+p1 = Calculadora
+p1.main()
